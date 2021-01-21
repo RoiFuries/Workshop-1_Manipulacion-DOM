097e2a4171622c4f4a6d0aa4ad59f98ca08e4c28
RoiFuries <roi.furies@gmail.com>
2021-01-20 21:46:09 -0600

    we connect to an api, we made a request for data and with a cycle we render them, or so i think

--- src/index.js
+++ src/index.js
@@ -3,4 +3,37 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo"
+
+// web API llamada fetch
+// cosas que hacer:
+// 1.- conectarnos al server
+// si queremos mejorar esta cosa se puede usar asyn await en lugar de promesas
+window
+.fetch(url)
+// 2.- Procesar la respuesta y convertirla en JSON
+.then(respuesta => respuesta.json())
+// 3.- cuando tengamos JSON tendremos data y esa info es la usamos pare renderizar la info en el browser
+.then(responseJson => {
+    const todosLosItems = [];
+    responseJson.data.forEach(item => {
+        // cuando estoy dentro del data.foreEach por cada uno de los items entonces voy a:
+        //crear imagen
+        const imagen = document.createElement('img');
+        //crear titulo
+        const titulo = document.createElement('h2');
+        document.body.appendChild(titulo)
+        // crear precio
+        const price = document.createElement('div');
+//aho un contenedor para esto
+        const container  = document.createElement('div')
+        container.append(imagen, titulo, price)
+
+        // container.appendChild(imagen)
+        // container.appendChild(titulo)
+        // container.appendChild(price)
+        todosLosItems.push(container)
+    })
+    document.body.append(...todosLosItems)
+
+})
