620704c2fbf02de0eb09e5e661e1c72e7a7271c8
RoiFuries <roi.furies@gmail.com>
2021-01-22 15:55:25 -0600

    obtuvimos los datos del API, experimentamos con estilos y clases y con la API de internalizacion del navegador

--- src/index.js
+++ src/index.js
@@ -3,14 +3,45 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-const url = "https://platzi-avo.vercel.app/api/avo"
+
+
+
+const url = "https://platzi-avo.vercel.app"
+// !!!! ojo esto de arriba despues se corrigio, otra solucion que implemente fue dejar la api completa  asi: const url = "https://platzi-avo.vercel.app/api/avo" 
+// y puedes hacer otra constante con la url absoluta algo asi:  const urlAbsoluta = "https://platzi-avo.vercel.app" y cuando llames 
+// a la imagen se hace un templete literal juntas la urlAbsoluta con el item del API. algo asi:
+//  imagen.src = `${urlAbsoluta}${item.image}` o puedes poner no crear la constante de la url absoluta y meterla directo en el templete literal asi:
+// imagen.src = `https://platzi-avo.vercel.app${item.image}` same shit
+
+
+const appNode = document.querySelector('#app')  //aqui lo que estamos haciendo es que appNose apunte al elemento en el html que con el id app
+
+
+// la api para internacionalizacion se encuentra en window.Int y ponemos que que vamos a usar monedas o fechas
+const fotmarPricee = (price) => {
+
+    const newprice = new window.Intl.NumberFormat('en-EN', {  //aqui  ponemos como primer parametro los locals osea el pais donde esta el usuario, para usar la notacion del lugar
+        //por ejemplo podemos poner en
+        style: 'currency', // como segundo parametro le pones que queremos que tengo un estio de moneda
+        currency: 'USD' // y como monedad ponemos el la moneda que queremos usar ejemplo  GBP MSN YJP
+    }).format(price)
+
+    return newprice
+}
+
+//intl se usa para:
+// 1.- formato a fechas
+// 2./ formato a monedas
+
+
+
 
 // web API llamada fetch
 // cosas que hacer:
 // 1.- conectarnos al server
-// si queremos mejorar esta cosa se puede usar asyn await en lugar de promesas
-window
-.fetch(url)
+// !!!! si queremos mejorar esta cosa se puede usar asyn await en lugar de promesas
+window   // con esto nos conectamos al servicor window.fetch
+.fetch(`${url}/api/avo`)
 // 2.- Procesar la respuesta y convertirla en JSON
 .then(respuesta => respuesta.json())
 // 3.- cuando tengamos JSON tendremos data y esa info es la usamos pare renderizar la info en el browser
@@ -18,22 +49,46 @@ window
     const todosLosItems = [];
     responseJson.data.forEach(item => {
         // cuando estoy dentro del data.foreEach por cada uno de los items entonces voy a:
-        //crear imagen
+        //oara llamar imagen  siempre necesitas un src por eso ponemos imagen(que hace referenia a const imagen).src y le dices que le asigne lo que trae del API con 
+        //item.image pero lo haces con un templete literal para qe los concatene ver arriba cuando grabamos la APi en constantes.
         const imagen = document.createElement('img');
-        //crear titulo
-        const titulo = document.createElement('h2');
-        document.body.appendChild(titulo)
-        // crear precio
-        const price = document.createElement('div');
-//aho un contenedor para esto
-        const container  = document.createElement('div')
-        container.append(imagen, titulo, price)
+        imagen.src = `${url}${item.image}`
 
+        // para poner imagen, las imagenes siempre tienen el la propiedad src osea pondriamo imagen.src 
+
+    //crear titulo
+        const titulo = document.createElement('h2');
+        titulo.textContent = item.name         // aqui le dice que queremos que el título sea igual contenido en texto que tiene en el
+        // item con el nombre name (el item hace referenca al item que pusimos en el forEach puedes poner cualquier cosa como tlacuache 
+        //solo que cuando pidas cosas tendras que hacerlo con ese nombre osea tlacuache.name, name no cambia porque asi esta en el api )
+
+        //Estilos
+        // para dar estilos se pueden hacer varias formas asi:
+        // como  cadena de texto >>>>  titulo.style = 'font-size: 2rem';
+        // este es el otro metodo >>>>> titulo.style.fontSize = '3rem'; 
+        // y creo que este es el mas prolijo usando clases con classNAme asi:
+        titulo.className = 'NomeDeLaClase';
+        // titulo.className = 'text-lg';
+
+
+
+
+
+    // crear precio
+        const precio = document.createElement('div');
+        // precio.textContent = item.price;  //precio quiero que el contenido que tu tienes como texto (.textContent) sea igual al item.price (el .price viene del api) 
+        precio.textContent = fotmarPricee(item.price);
+    
+        //aho un contenedor para esto
+        const container  = document.createElement('div')
+        container.append(imagen, titulo, precio)
+
+        //lo de arriba es lo mismo que aqui abajosolo que mas corto
         // container.appendChild(imagen)
         // container.appendChild(titulo)
         // container.appendChild(price)
         todosLosItems.push(container)
     })
-    document.body.append(...todosLosItems)
+    appNode.append(...todosLosItems)
 
 })
